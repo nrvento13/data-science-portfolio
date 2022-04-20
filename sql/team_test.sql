f0e598cf1d74eef56db11bad6d70ca5998bf5025
Noah Vento <noahvento@Noahs-MBP-2.attlocal.net>
2022-04-20 17:51:56 -0500

    Initial sql test

---
+++ sql/team_test.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS united_states (
+	first_name CHAR,
+	last_name CHAR,
+	birthplace CHAR,
+	height_in INT,
+	nationality CHAR,
+	club_team CHAR,
+	club_league CHAR,
+	position CHAR
+	);
+
